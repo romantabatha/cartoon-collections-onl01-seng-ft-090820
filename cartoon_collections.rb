422a004fb6a4d0cb1cedf5182ce1380883938c59
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 19:02:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,15 +2,15 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, i|
    puts "#{i+1}. #{name}"
   end
-  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planet)
+  planet.collect do |call|
+    call.capitalize+"!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
 end
 
 def find_the_cheese# code an argument here
