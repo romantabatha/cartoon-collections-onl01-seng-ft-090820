383d0345bb72f37c5fd2831c4c13c560b4ab5b8c
Tabatha Roman <romantabatha17@gmail.com>
2020-09-12 19:05:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,14 @@ def summon_captain_planet(planet)
   end
 end
 
-def long_planeteer_calls
+def long_planeteer_calls(word)
+  answer = false
+  word.each do |call|
+    if call.length > 4
+      answer = true
+    end
+  end
+  answer
 end
 
 def find_the_cheese# code an argument here
